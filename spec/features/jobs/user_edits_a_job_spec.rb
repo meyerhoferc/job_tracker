72f6f9232cb6f50288a37680c99d002e9ff00220
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-02-11 12:05:21 -0700

    updates job feature spec

--- spec/features/jobs/user_edits_a_job_spec.rb
+++ spec/features/jobs/user_edits_a_job_spec.rb
@@ -4,9 +4,11 @@ describe "user edits an existing job" do
   scenario "a user can edit a job" do
     company = Company.create!(name: "StackOverflow")
     job = company.jobs.create!(title: "Rails Developer",
-      description: "do rails things", level_of_interest: 3, city: "Denver")
+                               description: "do rails things",
+                               level_of_interest: 3,
+                               city: "Denver")
 
-    visit edit_company_job_path({id: job.id, company_id: company.id})
+    visit edit_company_job_path(id: job.id, company_id: company.id)
     fill_in("job[title]", with: "Backend Engineer")
     fill_in("job[description]", with: "do database things")
     click_on("Update Job")
