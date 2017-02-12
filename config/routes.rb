fdec5fbcb97cad115d2aeb3da2b2f2e5e7f30810
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-02-12 13:18:25 -0700

    updates routes file

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :categories, only: [:new, :create, :show, :index, :edit, :update, :destroy]
+  resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
